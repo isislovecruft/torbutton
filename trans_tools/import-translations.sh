18aba4a50c33bde95ec839bf1fbc48e0bbea769a
Mike Perry <mikeperry-git@torproject.org>
2014-02-13 16:43:23 +0000

    Bug 10611: Add Sweedish (sv) to extra locales to update.

--- trans_tools/import-translations.sh
+++ trans_tools/import-translations.sh
@@ -9,7 +9,7 @@ BUNDLE_LOCALES="ar de es fa fr it ko nl pl pt ru vi zh-CN"
 # so Basque XPIs can be build independently. We can do
 # this for other languages too, if anyone requests this
 # and translations are available.
-BUNDLE_LOCALES="$BUNDLE_LOCALES eu ja"
+BUNDLE_LOCALES="$BUNDLE_LOCALES eu ja sv"
 
 if [ -d translation ];
 then
